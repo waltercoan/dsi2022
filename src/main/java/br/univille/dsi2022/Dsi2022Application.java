d21aa8964241fb3044d24872e8573777c2bd5eae
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-05-03 19:26:44 -0300

    first commit

---
+++ src/main/java/br/univille/dsi2022/Dsi2022Application.java
@@ -0,0 +1,13 @@
+package br.univille.dsi2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsi2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsi2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/univille/dsi2022/Dsi2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.dsi2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsi2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
