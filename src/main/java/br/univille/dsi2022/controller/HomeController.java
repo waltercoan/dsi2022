1c153163c29476e9d42a5f7d49ff8de86284b04d
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-05-17 21:32:21 -0300

    configuracao thymeleaf e sqlserver

--- src/main/java/br/univille/dsi2022/controller/HomeController.java
+++ src/main/java/br/univille/dsi2022/controller/HomeController.java
@@ -1,15 +1,24 @@
 package br.univille.dsi2022.controller;
 
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping({"/","/home"})
 public class HomeController {
     
-    @GetMapping("/")
-    @ResponseBody
-    public String index(){
-        return "EU NAO ACREDITO!!!";
+    @GetMapping()
+    public ModelAndView index(){
+        var nome = "zezinho";
+        var sobrenome = "da Silva";
+        HashMap<String,String> mapa_de_dados = new HashMap<>();
+        mapa_de_dados.put("nomenatela",nome);
+        mapa_de_dados.put("sobrenomenatela",sobrenome);
+        return new 
+            ModelAndView("home/index",mapa_de_dados);
     }
 }
