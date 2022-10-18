0d8db29dc86ea95455ddce65219519a5260fab5a
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-10-18 20:58:20 -0300

    implementação CRUD API REST

--- src/main/java/br/univille/dsi2022/api/PacienteControllerAPI.java
+++ src/main/java/br/univille/dsi2022/api/PacienteControllerAPI.java
@@ -5,7 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import br.univille.dsi2022.dto.PacienteDTO;
@@ -22,4 +27,56 @@ public class PacienteControllerAPI {
         List<PacienteDTO> listaPacientes = service.getAll();
         return new ResponseEntity<>(listaPacientes,HttpStatus.OK);
     }
+
+    @PostMapping
+    public ResponseEntity<PacienteDTO> 
+            save(@RequestBody PacienteDTO paciente){
+        if(paciente.getId() == 0){
+            paciente = service.save(paciente);
+            return new 
+                ResponseEntity<PacienteDTO>(paciente,HttpStatus.CREATED);
+        }
+        return ResponseEntity.badRequest().build();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<PacienteDTO> 
+        getById(@PathVariable("id") long id){
+        var umPaciente = service.findById(id);
+        if(umPaciente.getId() == 0){
+            return ResponseEntity.notFound().build();
+        }
+        return 
+            new ResponseEntity<PacienteDTO>(umPaciente,
+                HttpStatus.OK);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<PacienteDTO> 
+            update(@PathVariable("id") long id,
+            @RequestBody PacienteDTO paciente){
+        var pacienteAntigo 
+                = service.findById(id);
+        if(pacienteAntigo.getId() == 0){
+            return ResponseEntity.notFound().build();
+        }
+        pacienteAntigo.setNome(paciente.getNome());
+        pacienteAntigo.setSexo(paciente.getSexo());
+        pacienteAntigo.setDataNascimento(paciente.getDataNascimento());
+        
+        pacienteAntigo = service.save(pacienteAntigo);
+        return new 
+            ResponseEntity<PacienteDTO>(pacienteAntigo, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<PacienteDTO> 
+        delete(@PathVariable("id") long id){
+        var pacienteAntigo = service.findById(id);
+        if(pacienteAntigo.getId() == 0){
+            return ResponseEntity.notFound().build();
+        }
+        service.delete(id);
+        return new 
+            ResponseEntity<PacienteDTO>(pacienteAntigo, HttpStatus.OK);
+    }
+
 }
