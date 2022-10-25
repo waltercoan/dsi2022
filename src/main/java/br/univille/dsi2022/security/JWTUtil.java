b11635123731adac2de2e98e03c9533cb69d5b2c
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-10-25 20:30:18 -0300

    WIP: JWT

--- src/main/java/br/univille/dsi2022/security/JWTUtil.java
+++ src/main/java/br/univille/dsi2022/security/JWTUtil.java
@@ -2,7 +2,9 @@ package br.univille.dsi2022.security;
 
 import java.util.Date;
 import java.util.Map;
+import java.util.function.Function;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
@@ -12,6 +14,32 @@ import io.jsonwebtoken.SignatureAlgorithm;
 public class JWTUtil {
     /*não fazer isso no mundo real */
     private String SECRET_KEY = "78-UTGE9JD342AESasf";
+
+    public boolean validateToken(String token,
+                                UserDetails userDetails){
+        String userName = extractUserName(token);
+        return ((userName.equals(userDetails.getUsername())) 
+                && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private String extractUserName(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+    
+    private <T> T extractClaim(String token, 
+                Function<Claims,T> claimsResolver){
+        Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
 
     private Claims extractAllClaims(String token){
         return Jwts.parser()
